f51faaa04f42509d5b97d3343d80c928520892a3
John A. Squier <jasquier@gmail.com>
2024-12-19 11:38:21 -0500

    test: exclude type definition files from coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     coverage: {
-      exclude: [".eslint*", "*.config.{ts,js}", "src/main.tsx"],
+      exclude: ["**/*.d.ts", ".eslint*", "*.config.{ts,js}", "src/main.tsx"],
     },
     environment: "jsdom",
     setupFiles: ["./testSetup.ts"],
